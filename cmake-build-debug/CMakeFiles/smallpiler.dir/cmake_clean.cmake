0284f0263af7db6ecfb3da83d74d18f692e7941d
Thomas Lavend'Homme <lavendthomas@outlook.be>
2019-10-10 20:00:49 -0400

    Add CMake
    Signed-off-by: Thomas Lavend'Homme <lavendthomas@outlook.be>

---
+++ cmake-build-debug/CMakeFiles/smallpiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smallpiler.dir/petit-comp.c.o"
+  "smallpiler.pdb"
+  "smallpiler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smallpiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
